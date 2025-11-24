52e424a799c0cd0e8d97f7c744d14613f9572c2f
Materdelo <olszowy.mateusz11@gmail.com>
2025-11-24 22:00:30 +0100

    Fix UI

--- NF04012506SG/app/src/main/java/com/example/nf04_01_2506_sg/MainActivity.java
+++ NF04012506SG/app/src/main/java/com/example/nf04_01_2506_sg/MainActivity.java
@@ -10,7 +10,11 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         seekR = findViewById(R.id.seekR);
         seekG = findViewById(R.id.seekG);
